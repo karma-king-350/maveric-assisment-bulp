443866b31cec1d38e07b00d5af4c10e5be75cf92
karma-king-350 <58129582+karma-king-350@users.noreply.github.com>
2021-04-16 20:02:47 +0530

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -4,12 +4,12 @@
      var image1 = document.getElementById('Image1');
     
      if (image.src.match("bulpon"))
-         image.src = "images/bulpof.jpg";
+         image.src = "bulpof.jpg";
      else
          image.src = "bulpon.jpg";
    
      if (image1.src.match("bulpof"))
-         image1.src = "images/bulpon.jpg";
+         image1.src = "bulpon.jpg";
      else
          image1.src = "bulpof.jpg";
     }
